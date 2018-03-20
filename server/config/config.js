b8a77e43d5dfd7f49b06c9dd8e320515fb54edc8
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2018-03-20 14:21:26 +0530

    connection string

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,6 @@
 module.exports.setConfig = function(){
-    process.env.CONNECTIONSTRING = 'mongodb://localhost/RecognitionSystem';
+    process.env.CONNECTIONSTRING = 'mongodb://myPpdUser:83DySOEj85scornl@cluster0-shard-00-00-x27qu.mongodb.net:27017,cluster0-shard-00-01-x27qu.mongodb.net:27017,cluster0-shard-00-02-x27qu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
     process.env.SECRET_KEY = 'piakey';
+
+    
 }
